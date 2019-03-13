3b6a181fdd824ed96031a9465ea08d03ff2d802e
Justin Reich <justinreich.dev@gmail.com>
2019-03-13 15:32:18 -0500

    Revert "add translate locally"
    This reverts commit 3babf39204763409f81b2cb5476da4c75fe59d62.

--- src/main/java/com/codeup/springcontacts/ContactController.java
+++ src/main/java/com/codeup/springcontacts/ContactController.java
@@ -1,17 +1,10 @@
 package com.codeup.springcontacts;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
@@ -20,27 +13,6 @@ public class ContactController {
 
     public ContactController(ContactRepository contactDao) {
         this.contactDao = contactDao;
-    }
-
-    @GetMapping("/translate/{word}")
-    @ResponseBody
-    public String translateToGerman(@PathVariable String word, @Value("${GOOGLE_APPLICATION_CREDENTIALS}") String gJson, @Value("${GOOGLE_CLOUD_PROJECT}") String gProj) {
-        Translate trans = null;
-        try {
-            trans = TranslateOptions.newBuilder()
-                    .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(gJson)))
-                    .setProjectId(gProj)
-                    .build()
-                    .getService();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Translation translation =
-                trans.translate(
-                        word,
-                        Translate.TranslateOption.sourceLanguage("en"),
-                        Translate.TranslateOption.targetLanguage("ger"));
-        return translation.getTranslatedText();
     }
 
     @GetMapping({"/", "/contacts"})
